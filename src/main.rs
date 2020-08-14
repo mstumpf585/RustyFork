2434e1924c59aca9538f79b7371610a45d43f348
Michael Stumpf <michaelstumpf585@gmail.com>
2020-08-13 23:32:31 -0400

    adding template engine

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-mod requestHandler;
+use actix_files as fs;
+use actix_web::{web, App, HttpResponse, HttpRequest, HttpServer, Responder};
+use tera::{Tera, Context}; 
+pub mod requestHandler;
 
+/*
+* This is the main entry point 
 
+*/
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+
+        // Put our generator to the folder location of the templates. 
+        let tera = Tera::new(
+            concat!(env!("CARGO_MANIFEST_DIR"), "/template/**/*")
+        ).unwrap(); 
+
         App::new()
-            .route("/", web::get().to(requestHandler::index))
+            .data(requestHandler::AppData {tmpl: tera})
+            .route("/hello", web::get().to(requestHandler::index))
             .route("/again", web::get().to(requestHandler::index2))
+            .service(requestHandler::index3)
+            .service(web::resource("tmpl/{name}").route(web::get().to(requestHandler::index4)))
     })
     .bind("127.0.0.1:8088")?
     .run()
